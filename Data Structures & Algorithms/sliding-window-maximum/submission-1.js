002b80d8e985dbcf512f553f75346c7e90f7d923
muhamed-hossam2001 <112786826+muhamed-hossam2001@users.noreply.github.com>
2026-07-14 09:01:49 +0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        const q =[]
+        const output =[]
+        let l = 0
+        let r =0
+
+        while(r<nums.length){
+            while(q.length && nums[q[q.length-1]] <nums[r]){
+                q.pop()
+            }
+            q.push(r)
+
+            if(l>q[0]){
+                q.shift()
+            }
+            if(r+1>=k){
+                output.push(nums[q[0]])
+                l++
+            }
+            r++
+        }
+        return output
+    }
+}
