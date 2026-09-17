2e0a7e984afbce30f899a67b8d436793c04034f6
muhamed-hossam2001 <112786826+muhamed-hossam2001@users.noreply.github.com>
2026-09-17 10:05:30 +0400

    Add: median-of-two-sorted-arrays - submission-4

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-4.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    findMedianSortedArrays(nums1, nums2) {
+    const total = nums1.length + nums2.length
+    const half = Math.floor(total/2)
+
+    const A = nums1.length > nums2.length ? nums2 : nums1
+    const B = nums1.length > nums2.length ? nums1 : nums2
+
+    let l = 0 
+    let r = A.length-1
+
+    while(true){
+        const i = Math.floor((l+r)/2)
+        const j = half - i -2 
+
+        const Aleft = i>=0 ? A[i] : -Infinity
+        const Aright = i<A.length-1 ? A[i+1] : Infinity
+        const Bleft = j>=0 ? B[j] : -Infinity
+        const Bright = j<B.length-1 ? B[j+1] : Infinity
+
+
+        if(Aleft<=Bright && Bleft <=Aright){
+            if(total%2){
+                return Math.min(Aright,Bright)
+            }else{
+                return (Math.max(Aleft,Bleft)+Math.min(Aright,Bright))/2
+            }
+        }else if (Aleft>Bright){
+            r=i-1
+        }else{
+            l=i+1
+        }
+    }
+
+
+    }
+}
